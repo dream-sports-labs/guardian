a4788b69899d35080e4462960afc41fad0c8853d
pranavreddyd11 <pranav.reddy@dream11.com>
2025-06-24 16:40:30 +0530

    changed implementation flow APis to Flows

--- src/test/java/com/dreamsportslabs/guardian/utils/ApplicationIoUtils.java
+++ src/test/java/com/dreamsportslabs/guardian/utils/ApplicationIoUtils.java
@@ -105,23 +105,23 @@ public class ApplicationIoUtils {
     return execute(null, headers, new HashMap<>(), spec -> spec.get("/v1/certs"));
   }
 
-  public static Response blockContactApis(String tenantId, Map<String, Object> body) {
+  public static Response blockContactFlows(String tenantId, Map<String, Object> body) {
     Map<String, String> headers = new HashMap<>();
     headers.put(HEADER_TENANT_ID, tenantId);
     headers.put(CONTENT_TYPE, "application/json");
 
-    return execute(body, headers, new HashMap<>(), spec -> spec.post("/contact/block"));
+    return execute(body, headers, new HashMap<>(), spec -> spec.post("/v1/contact/block"));
   }
 
-  public static Response unblockContactApis(String tenantId, Map<String, Object> body) {
+  public static Response unblockContactFlows(String tenantId, Map<String, Object> body) {
     Map<String, String> headers = new HashMap<>();
     headers.put(HEADER_TENANT_ID, tenantId);
     headers.put(CONTENT_TYPE, "application/json");
 
-    return execute(body, headers, new HashMap<>(), spec -> spec.post("/contact/unblock"));
+    return execute(body, headers, new HashMap<>(), spec -> spec.post("/v1/contact/unblock"));
   }
 
-  public static Response getBlockedApis(String tenantId, String contactId) {
+  public static Response getBlockedFlows(String tenantId, String contactId) {
     Map<String, String> headers = new HashMap<>();
     headers.put(HEADER_TENANT_ID, tenantId);
 
@@ -129,6 +129,6 @@ public class ApplicationIoUtils {
         null,
         headers,
         new HashMap<>(),
-        spec -> spec.get("/contact/" + contactId + "/blocked-apis"));
+        spec -> spec.get("/v1/contact/" + contactId + "/blocked-flows"));
   }
 }
