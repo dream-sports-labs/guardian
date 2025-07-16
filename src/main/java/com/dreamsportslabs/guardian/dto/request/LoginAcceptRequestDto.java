76d01f498256b414e31e23c6ce1a24468bbf9764
Sesha Chintala <chintala.sai@dream11.com>
2025-07-16 23:32:01 +0530

    feat: added api for login accept - oidc

---
+++ src/main/java/com/dreamsportslabs/guardian/dto/request/LoginAcceptRequestDto.java
@@ -0,0 +1,21 @@
+package com.dreamsportslabs.guardian.dto.request;
+
+import static com.dreamsportslabs.guardian.exception.ErrorEnum.INVALID_REQUEST;
+
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+@Data
+public class LoginAcceptRequestDto {
+  private String loginChallenge;
+  private String refreshToken;
+
+  public void validate() {
+    if (loginChallenge == null || StringUtils.isBlank(loginChallenge)) {
+      throw INVALID_REQUEST.getCustomException("login_challenge is required");
+    }
+    if (refreshToken == null || StringUtils.isBlank(refreshToken)) {
+      throw INVALID_REQUEST.getCustomException("refresh_token is required");
+    }
+  }
+}
